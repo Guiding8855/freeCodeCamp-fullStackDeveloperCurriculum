d9fe6b308d82a800709f5eedf8d366015ac3bf81
Stuart Bell <stuart.andrew.bell@gmail.com>
2025-04-06 11:19:51 +1000

    Add first labs code to repo

---
+++ 04_JavaScript/03_sentenceMaker/script.js
@@ -0,0 +1,26 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "gigantic";
+noun = "chair";
+verb = "throwing";
+place = "castle";
+adjective2 = "miniscule";
+noun2 = "pastry";
+
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+console.log("First story: " + firstStory);
+
+adjective = "wimpy";
+noun = "box";
+verb = "dancing";
+place = "9th circle of Hell";
+adjective2 = "shiny";
+noun2 = "lamp";
+
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb  + ".";
+console.log("Second story: " + secondStory);
