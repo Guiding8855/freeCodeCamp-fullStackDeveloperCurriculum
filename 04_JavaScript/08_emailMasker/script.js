a51fc295529a78e7c2e0af6df0605a9c046e7fda
Stuart Bell <stuart.andrew.bell@gmail.com>
2025-04-10 20:59:11 +1000

    Complete emailMasker code

--- 04_JavaScript/08_emailMasker/script.js
+++ 04_JavaScript/08_emailMasker/script.js
@@ -1,19 +1,19 @@
 let email = "fakeEmail@domain.com";
 
-console.log(email.length)
-
 function maskEmail(email) {
 // separate address and domain parts
   let origAddress = email.slice(0, email.indexOf("@"));
-  console.log(origAddress);
   let domain = email.slice(email.indexOf("@"), email.length);
-  console.log(domain);
 
 // mask the address part
-let maskedAddress = origAddress[0] + "*".repeat(origAddress.length - 2) + origAddress[origAddress.length];
-console.log(maskedAddress);
+  let maskedAddress = origAddress[0] + "*".repeat(origAddress.length - 2) + origAddress[origAddress.length - 1];
 
 // concatenate the masked address and domain, and return
+  let maskedEmail = maskedAddress + domain;
+
+  return maskedEmail;
 };
 
+console.log(maskEmail("apple.pie@example.com"));
+console.log(maskEmail("freecodecamp@example.com"));
 console.log(maskEmail(email));
