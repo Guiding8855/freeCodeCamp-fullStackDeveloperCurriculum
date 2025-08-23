7dde8d7a75b6299ec1712bbf32fd8dd3f45cc77d
Stuart Bell <stuart.andrew.bell@gmail.com>
2025-08-23 21:14:23 +1000

    Add booleanCheck code

---
+++ 04_JavaScript/07.5_booleanCheck/script.js
@@ -0,0 +1,17 @@
+function booWho(val) {
+  if (val === true || val === false) {
+    return true
+  }
+  else return false
+};
+
+console.log(booWho(true));
+console.log(booWho(false));
+console.log(booWho([1,2,3]))
+console.log(booWho([].slice))
+console.log(booWho({ "a" : 1 }))
+console.log(booWho(1))
+console.log(booWho(NaN))
+console.log(booWho("a"))
+console.log(booWho("true"))
+console.log(booWho("false"))
