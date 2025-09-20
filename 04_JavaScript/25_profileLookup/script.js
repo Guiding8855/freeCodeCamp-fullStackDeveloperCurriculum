ff2ec1f31a5980a089aa156633cfdb6c84d161d1
Stuart Bell <stuart.andrew.bell@gmail.com>
2025-09-21 09:28:03 +1000

    Add profile lookup lab code

---
+++ 04_JavaScript/25_profileLookup/script.js
@@ -0,0 +1,54 @@
+let contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, property) {
+  let result;
+
+  for (const entry in contacts) {
+    if (contacts[entry]["firstName"] === name) {
+      if (contacts[entry].hasOwnProperty(property)) {
+        result = contacts[entry][property];
+        break;
+      } else {
+        result = "No such property";
+        break;
+      }
+    } else {
+      result = "No such contact";
+    }
+  }
+
+  return result;
+};
+
+// tests
+console.log(lookUpProfile("Akira", "lastName")); // Laine
+console.log(lookUpProfile("Kristian", "lastName")); // Vos
+console.log(lookUpProfile("Sherlock", "likes")); // [ 'Intriguing Cases', 'Violin' ]
+console.log(lookUpProfile("Harry", "likes")); // [ 'Hogwarts', 'Magic', 'Hagrid' ]
+console.log(lookUpProfile("Bob", "number")); // No such contact
+console.log(lookUpProfile("Akira", "address")); // No such property
